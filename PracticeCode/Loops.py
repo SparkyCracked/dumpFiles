b075ce5c61e2fb65b44dbf5ce8486c6d98881aac
SparkyCracked <106302821+SparkyCracked@users.noreply.github.com>
2022-05-26 12:41:00 +0200

    Add files via upload

---
+++ PracticeCode/Loops.py
@@ -0,0 +1,13 @@
+
+if __name__ == '__main__':
+    n = int(input())
+    i = 0
+
+    while i < n:
+        print( i *i)
+        i = i+ 1
+
+
+#Task
+#The provided code stub reads and integer, n,
+# from STDIN. For all non-negative integers i<n, print i^2.
